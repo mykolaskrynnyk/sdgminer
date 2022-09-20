abd89a0e50a7e1f8224e4828a0a346f9ba6a7fb2
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2022-09-20 17:03:47 +0300

    Refactor(transformations): remove get_sdg_features* functions and added SalienceRecord support

--- src/sdgminer/transformations.py
+++ src/sdgminer/transformations.py
@@ -15,12 +15,13 @@ import pandas as pd
 import networkx as nx
 
 # local packages
-from .utils import sdg_id2name, sdg_id2color, listify
+from .entities import SalienceRecord
+from .utils import SDGConverter, listify
 
 
-def calculate_sdg_salience(y_pred: List[Dict[int, float]]) -> Dict[int, float]:
+def calculate_sdg_salience(y_pred: List[Dict[int, float]]) -> SalienceRecord:
     """
-    Calculate the number of texts predicted for each sdg and normalise by the most largest number.
+    Calculate the number of texts predicted for each sdg and normalise by the largest number.
 
     Parameters
     ----------
@@ -29,7 +30,7 @@ def calculate_sdg_salience(y_pred: List[Dict[int, float]]) -> Dict[int, float]:
 
     Returns
     -------
-    sdg2salience : Dict[int, float]
+    salience : SalienceRecord
         A mapping from sdg ids to relative salience.
     """
     counter = Counter()
@@ -40,178 +41,8 @@ def calculate_sdg_salience(y_pred: List[Dict[int, float]]) -> Dict[int, float]:
 
     # normalise
     maximum = max(counter.values()) if len(counter) > 1 else 1
-    sdg2salience = {k: counter.get(k, 0) / maximum for k in range(1, 18)}
-    return sdg2salience
-
-
-def get_sdg_features(df_texts: pd.DataFrame, sdg2features: Dict[int, List[str]]) -> Dict[int, List[str]]:
-    """
-    Get most common features per SDG using pre-defined SDG features.
-
-    Calculates most common features (tokens/phrases) for each group of SDG texts. In so doing, the function considers
-    only features from a pre-defined list of terms for each SDG. It only uses the predictions from a multilabel model.
-
-    Parameters
-    ----------
-    df_texts : pd.DataFrame
-        A dataframe of containing a 'text_' column and columns with predicted sdgs.
-    sdg2features : Dict[int, List[str]]
-        A mapping from integer numbers (sdg ids) to a list of strings that define features relevant for to this sdg.
-
-    Returns
-    -------
-    sdg_id2top_features : Dict[int, List[str]]
-        A mapping from integer numbers (sdg ids) to a list of strings that define top features sorted in descending
-        order of importance. Does not include any sdg for which no text was assigned.
-    """
-    assert 'sdgs_multilabel' in df_texts.columns, '"sdgs_multilabel" column in missing'
-
-    df_sdgs = df_texts.dropna(subset = ['sdgs_multilabel']) \
-        .explode('sdgs_multilabel') \
-        .groupby('sdgs_multilabel') \
-        .agg({'text_': lambda x: ' '.join(list(x))})
-
-    sdg_id2top_features = dict()
-    for sdg in df_sdgs.index:
-        # top predictors from a multiclass logistic regression model, excludes uni-grams
-        features = [feature for feature in sdg2features[sdg] if ' ' in feature]
-        text = df_sdgs.loc[sdg, 'text_']
-        feature2frequency = {feature: text.count(feature) for feature in features}
-        top_features = [
-            feature for feature, frequency in sorted(feature2frequency.items(), key = lambda x: x[1], reverse = True)
-            if frequency > 0  # exclude zero frequency features
-        ]
-        sdg_id2top_features[sdg] = top_features  # may be an empty list
-    return sdg_id2top_features
-
-
-def get_sdg_features_pairwise(df_texts: pd.DataFrame, sdg2features: Dict[int, List[str]]) -> Dict[Tuple[int], List[str]]:
-    """
-    Get most common features for all pairs of sdgs that co-occur.
-
-    Calculates most common features for each pair of sdgs from texts that are assigned to more than one sdg. In theory,
-    this is expected to correspond to mixed-sdg topics, i.e., "sdg synergies". The function considers only features
-    from a pre-defined list of terms for each SDG. It only uses the predictions from a multilabel model.
-
-    Parameters
-    ----------
-    df_texts : pd.DataFrame
-        A dataframe of containing a 'text_' column and columns with predicted sdgs.
-    sdg2features : Dict[int, List[str]]
-        A mapping from integer numbers (sdg ids) to a list of strings that define features relevant for to this sdg.
-
-    Returns
-    -------
-    sdg_ids2top_features: Dict[Tuple[str], List[str])
-        A mapping from each pair of co-occuring SDGs to a list of strings that define top features sorted in descending
-        order of importance. Does not include any sdg pair for which no text was assigned.
-    """
-    # subset only those texts that have more than 1 sdg label predicted
-    assert 'sdgs_multilabel' in df_texts.columns, '"sdgs_multilabel" column in missing'
-
-    df_texts = df_texts.loc[df_texts['sdgs_multilabel'].str.len().ge(2)].copy()
-    df_texts['sdgs'] = df_texts['sdgs_multilabel'] \
-        .apply(list) \
-        .apply(sorted) \
-        .apply(lambda x: list(combinations(x, 2)))  # consider all pairwise combinations
-    df_sdgs = df_texts.explode('sdgs') \
-        .groupby('sdgs', as_index = False) \
-        .agg({'text_': lambda x: ' '.join(list(x))})  # all pairwise combiations
-    df_sdgs['sdgs'] = df_sdgs['sdgs'].apply(tuple)  # each cell is a tuple of two sdgs
-
-    sdg_ids2top_features = dict()
-    for idx, row in df_sdgs.iterrows():
-
-        # combine features from both sdgs
-        features = sdg2features[row['sdgs'][0]] + sdg2features[row['sdgs'][1]]
-        features = [feature for feature in features if ' ' in feature]
-        feature2frequency = {feature: row['text_'].count(feature) for feature in features}
-        top_features = [
-            feature for feature, frequency in sorted(feature2frequency.items(), key = lambda x: x[1], reverse = True)
-            if frequency > 0  # exclude zero frequency features
-        ]
-        sdg_ids2top_features[row['sdgs']] = top_features[:15]  # keep only top 15 features for each pair
-    return sdg_ids2top_features
-
-
-def construct_sdg_graph(df_texts: pd.DataFrame, add_edge_features: bool = False, sdg2features: Dict[int, List[str]] = None) -> nx.Graph:
-    """
-    Construct a graph of relationships between SDGs in the texts.
-
-    Creates an nx.Graph using pairwise features from co-occurence of SDGs. The output of this function is used for
-    plotting.
-
-    Parameters
-    ----------
-    df_texts : pd.DataFrame
-        A dataframe of containing a 'text_' column and columns with predicted sdgs.
-    add_edge_features : bool, default=False
-        A flag to indicate whether co-occurrence keywords should be added as edge attributes.
-    sdg2features : Dict[int, List[str]]
-        A mapping from integer numbers (sdg ids) to a list of strings that define features relevant for to this sdg.
-
-    Returns
-    -------
-    G : nx.Graph
-        A graph of interconnections between SDGs that is used for plotting.
-    """
-    assert 'sdgs_multilabel' in df_texts.columns, '"sdgs_multilabel" column in missing'
-
-    X = np.zeros((17, 17))  # adjacency matrix with occurrence counts on the diagonal
-    for sdg_ids2probs in df_texts['sdgs_multilabel'].dropna().values:
-
-        # loop through each pairwise combination and count co-occurrence
-        for i, j in combinations(sdg_ids2probs.keys(), 2):
-            X[i-1, j-1] += 1
-            X[j-1, i-1] += 1
-
-        # add occurrence counts on the diagonal
-        for k in sdg_ids2probs.keys():
-            X[k-1, k-1] += 1 # salience of an SDG, i.e., occurrence count
-    assert (X == X.T).all(), 'X is an adjacency matrix, it must be symmetric.'
-
-    df_sdgs = pd.DataFrame(X, columns = range(1, 18), index = range(1, 18))
-    df_sdgs = df_sdgs.reset_index() \
-        .rename({'index': 'from'}, axis = 1) \
-        .melt(id_vars = 'from', var_name = 'to', value_name = 'weight')
-
-    # get unique pair ids to remove duplicates
-    df_sdgs['id'] = df_sdgs[['from', 'to']].astype(str).apply(list, axis = 1).apply(sorted).str.join('-')
-    df_sdgs.drop_duplicates(subset = 'id', inplace = True)
-    df_sdgs = df_sdgs.query('`from` != `to`').copy()  # removing edges to itself
-
-    # sanity check: total number of pairs minus the number of duplicated pairs minus self-edges to itself
-    assert df_sdgs.shape[0] == 17 * 17 - (17 * 17 - 17) / 2 - 17
-
-    # normalising the weight to the range from 0 to 10
-    df_sdgs['weight'] = df_sdgs['weight'].divide(df_sdgs['weight'].max()).multiply(10)
-    df_sdgs = df_sdgs.query(
-        expr = 'weight > @threshold and weight >= 5',
-        local_dict = {'threshold': df_sdgs['weight'].quantile(.5)}  # only keep edges whose weights are above the median
-    )
-
-    # constructing the graph
-    G = nx.Graph()
-    nodes = list(range(1, 18))  # always add all nodes
-    G.add_nodes_from(nodes)
-    edges = df_sdgs[['from', 'to', 'weight']].apply(tuple, axis = 1).tolist()
-    G.add_weighted_edges_from(edges)
-
-    # add edge attributes, if any are given
-    if add_edge_features:
-        sdgs2features = get_sdg_features_pairwise(df_texts, sdg2features)
-        edge_attributes = {k: {'features': listify(v)} for k, v in sdgs2features.items()}
-        nx.set_edge_attributes(G, edge_attributes)
-
-    # add node attributes
-    weight_max = np.diag(X).max()
-    for node in G.nodes:
-        G.nodes[node]['name'] = sdg_id2name[node]
-        # nodes are from 1 to 17, indices are from 0 to 16
-        G.nodes[node]['weight'] = X[node-1, node-1] / weight_max * 100  # scale the node weight
-        G.nodes[node]['color'] = sdg_id2color[node]
-
-    return G
+    salience = SalienceRecord(dictionary={k: counter.get(k, 0) / maximum for k in range(1, 18)})
+    return salience
 
 
 # TODO: improve the function to account for the fact that features are derived after preprocessing
