52d19a36e46259fe783807be0963116fb7d19f30
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2022-09-20 17:08:09 +0300

    Test(extraction): update the test in line with the new interface

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -15,11 +15,11 @@ def test_extract():
     text_source_sents = re.split(r'\n+|\.\s+', text_source)  # split titles and paragraphs into sentences
 
     # test typical one-column and two-column texts
-    test_files = ('lorem_ipsum_1.pdf', 'lorem_ipsum_2.pdf')
+    test_files = ('lorem_ipsum.txt', 'lorem_ipsum_1.pdf', 'lorem_ipsum_2.pdf')
+    text_extractor = TextExtractor()
 
     for file_name in test_files:
-        text_extractor = TextExtractor(filepath = os.path.join('tests', file_name))
-        text_extractor.extract_text()
+        text_extractor.extract_text(filepath = os.path.join('tests', file_name))
         text_extracted = re.sub(pattern  = r'\s+', repl = ' ', string = text_extractor.text)
         match_count = sum(1 for sent in text_source_sents if sent in text_extracted)
         overlap = match_count / len(text_source_sents)
